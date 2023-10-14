77861c06371b5bc4f41dbaa474941d54decb9a78
Brevan Mitchell <brevanm@gmail.com>
2023-10-13 22:50:05 -0400

    Basic about section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Showcase from "../components/Showcase";
 import ServiceCard from "../components/ServiceCard";
 import Form from "../components/Form";
 import TreeImage from "../assets/trees.jpg";
+import AboutImage from "../assets/about.png";
 import Button from "../components/Button";
 
 const HomePage = () => {
@@ -95,8 +96,31 @@ const HomePage = () => {
       </Showcase>
       <Showcase id="about" padding>
         <div className="flex flex-col items-center px-2 pt-24 pb-6">
-          <h1 className="font-bold text-6xl">About</h1>
-          <p>TODO</p>
+          <div className="flex flex-wrap gap-8 items-center justify-center">
+            <img
+              className="cover-image max-h-96 rounded-lg"
+              src={AboutImage}
+              alt="first choice loading mulch"
+            />
+            <div
+              style={{ maxWidth: "30rem" }}
+              className="text-center md:text-left"
+            >
+              <h3 className="text-green-800 text-xl my-4">
+                First Choice Lawn and Landscape, LLC
+              </h3>
+              <h1 className="font-bold text-3xl my-4">
+                We are Summit County's premier landscape maintenance partner.
+              </h1>
+              <p className="my-4">
+                At Leadway Landscaping, we strive to be leaders in the Green
+                Industry, setting the standard for quality, innovation, and
+                cultivating a strong team culture. With our commitment to
+                sustainability and a forward-thinking approach, we aim to make a
+                positive impact and pave the way for a greener future.
+              </p>
+            </div>
+          </div>
         </div>
       </Showcase>
       <Showcase id="contact" bgColor="#FAFDFB" padding>
