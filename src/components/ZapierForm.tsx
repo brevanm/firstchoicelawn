9d6c8e165cda8b701b25a354887ad9de01ab90a4
Brevan Mitchell <brevanm@gmail.com>
2024-01-26 21:55:24 -0500

    Careers page verbiage + zapier

---
+++ src/components/ZapierForm.tsx
@@ -0,0 +1,6 @@
+const ZapierForm = () => {
+    // @ts-ignore-next-line
+    return <zapier-interfaces-page-embed page-id='clrvfups4000481g30xi9k5bl' no-background='true'  style={{ height: '80rem'}}></zapier-interfaces-page-embed>
+};
+
+export default ZapierForm;
--- src/pages/CareersPage.tsx
+++ src/pages/CareersPage.tsx
@@ -1,6 +1,7 @@
 import showcase from "../assets/showcase.jpg";
 import CareersContactForm from "../components/CareersContactForm";
 import Showcase from "../components/Showcase";
+import ZapierForm from "../components/ZapierForm";
 
 const CareersPage = () => {
 
@@ -29,28 +30,36 @@ const CareersPage = () => {
           </h1>
           <p className="my-6 text-center text-white">
           First Choice Lawn & Landscape LLC is the perfect choice for workers looking for a company that values customer service and satisfaction. 
-          With over ten years of experience in the landscape construction and maintenance field, 
-          company owner Jayce is committed to providing a great experience from start to finish for our clients and employees alike.
+          With over five years of experience in the lawn and landscape industry, we are committed to providing a great experience for our clients and employees alike.
           </p>
         </div>
       </Showcase>
-      <Showcase padding>
-        <div className="flex flex-col items-start px-2 pt-24 pb-6 max-w-screen-xl m-auto">
+      <Showcase  padding>
+        <div className="flex flex-col items-start px-2 pt-24 pb-12 max-w-screen-xl m-auto">
             <h1 className="font-bold text-4xl">Why work with First Choice Lawn & Landscape?</h1>
             <hr className="w-24 h-1 bg-fcll-green rounded-full mt-4 border-none" />
             <p className="pt-6 w-full">
             First Choice Lawn & Landscape is a company in Summit County, OH, that is dedicated to providing its employees with a great work environment. 
-            We believe that our employees are our most important asset, and we strive to create an atmosphere that is both enjoyable and productive. 
-            After all, we believe that happy employees lead to a successful business.
-            </p>
-            <p className="pt-6 w-full">
-            If you are looking for a company that values its employees and offers a great work environment, then look no further than Innovative Lawn & Landscape. 
+            We believe that our employees are our most important asset, and we strive to create an atmosphere that is both enjoyable and productive. If you’re looking for a new place to call home, then look no further than First Choice Lawn & Landscape. 
             Get in touch with us to learn more about our company and how we can help you reach your career goals!
             </p>
         </div>
         </Showcase>
-        <Showcase bgColor="#FAFDFB" padding>
-          <CareersContactForm />
+      <Showcase bgColor="#FAFDFB" padding>
+        <div className="flex flex-col items-start px-2 pb-24 pt-12 max-w-screen-xl m-auto">
+            <h1 className="font-bold text-4xl">We {'>'} Me</h1>
+            <hr className="w-24 h-1 bg-fcll-green rounded-full mt-4 border-none" />
+            <p className="pt-6 w-full">
+            Since the inception of First Choice, company owner Jayce Dippel has strived to implement a culture called "We over Me." "We over Me", 
+            is the idea that while we are all individuals, our success is defined by how we perform as a collective. In doing so, First Choice has built a team-oriented culture that has proven crucial to our organizational success. 
+            It takes all of us working together to build lasting relationships with our clients and to help foster growth within our business. Together, we make progress.
+            </p>
+        </div>
+        </Showcase>
+        <Showcase bgColor="#54784A" padding>
+          {/* <CareersContactForm /> */}
+          <h1 className="font-bold text-4xl md:text-5xl text-white text-center mt-12">Reach out today!</h1>
+          <ZapierForm />
       </Showcase>
     </>
   );
